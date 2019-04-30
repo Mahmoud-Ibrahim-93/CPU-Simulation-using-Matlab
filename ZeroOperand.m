1044aa9d9d5bd72bd2f6172f8194b73e52b2856d
mahmoud Ibrahim <mahmoud0i0d@gmail.com>
2019-04-30 21:33:00 +0200

    Add comments and correct indentation for ZeroOperand function

--- ZeroOperand.m
+++ ZeroOperand.m
@@ -1,28 +1,22 @@
 function [registers,memory,IR ] = ZeroOperand(registers,memory,IR )
 % Executes the zero operand instruction
 
-
+% increment PC by 1
  registers(1)=registers(1)+1;
-
  switch IR
-
-     case 242 % RETURN 
+     case 242 % RETURN from a subroutine call. The return address is restored from the stack
+         % PC <= *SP (value of the top of the stack location is stored in the program counter PC)
          registers(1)=memory(registers(2)+1);
+         % inc rease stack pointer value (SP <= SP +1)
          registers(2)=registers(2)+1;
-
-     case 244 % INITSP 
-
+     case 244 % INITSP SP=255
          registers(2)=255; %%SP<==FF
-     case 254 % NOP 
+     case 254 % NOP No Operation
 %          continue; % NO Operation
-     
-
-     case 255 % RETURN 
-         IR=255;
-
+     case 255 % RETURN (Stop execution)
+%          IR=255;
      otherwise
          disp('can not recognise this zero instruction pattern');
-
  end
 end
 
