713dcbac4a5bd688eb8e17acc43177ca7d10b552
mahmoud Ibrahim <mahmoud0i0d@gmail.com>
2019-05-23 17:35:33 +0200

    minor bug fixes

--- myGUII.m
+++ myGUII.m
@@ -22,7 +22,7 @@ function varargout = myGUII(varargin)
 
 % Edit the above text to modify the response to help myGUII
 
-% Last Modified by GUIDE v2.5 22-May-2019 03:17:14
+% Last Modified by GUIDE v2.5 23-May-2019 17:31:17
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -398,3 +398,25 @@ function pauseDelay_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on mouse press over axes background.
+function axes1_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to axes2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- If Enable == 'on', executes on mouse press in 5 pixel border.
+% --- Otherwise, executes on mouse press in 5 pixel border or over jmp2End.
+function jmp2End_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to jmp2End (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes during object creation, after setting all properties.
+function jmp2End_Callback(hObject, eventdata, handles)
+% hObject    handle to jmp2End (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
